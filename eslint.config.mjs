91501e7f304e849c44a26510fadca2d0a0b8b05c
Horus Turboss <81076765+docteur-turboss@users.noreply.github.com>
2025-10-03 06:04:43 +0200

    🔧 Update ESLint config (modules, ignores)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { globalIgnores, defineConfig } from 'eslint/config'
 
 export default defineConfig([
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: { globals: globals.browser },
+    files: ['**/*.{ts,js,mjs,cjs}'],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.node,
+    },
   },
   tseslint.configs.recommended,
-]);
+])
