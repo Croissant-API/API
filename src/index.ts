dbebbaa9344b019be9d2f31a5ffd4597b8150cc0
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2025-10-21 21:52:48 +0200

    Refactor code structure for improved readability and maintainability

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,52 @@
 import { app } from './app';
 
-import { exec } from 'child_process';
-import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
+import { httpServerHandler } from 'cloudflare:node';
 
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT as string) || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
-function getTimestamp() {
-  const now = new Date();
-  const pad = (n: number) => n.toString().padStart(2, '0');
-  return `${pad(now.getDate())}-${pad(now.getMonth() + 1)}-${now.getFullYear()}_${pad(now.getHours())}-${pad(now.getMinutes())}-${pad(now.getSeconds())}`;
-}
+export default httpServerHandler({ port: port });
 
-function backupDatabase() {
-  const timestamp = getTimestamp();
-  const backupDir = path.join(process.cwd(), 'database_backups');
-  const backupPath = path.join(backupDir, `mysql_backup_${timestamp}.sql`);
 
-  if (!fs.existsSync(backupDir)) {
-    fs.mkdirSync(backupDir, { recursive: true });
-  }
+// function getTimestamp() {
+//   const now = new Date();
+//   const pad = (n: number) => n.toString().padStart(2, '0');
+//   return `${pad(now.getDate())}-${pad(now.getMonth() + 1)}-${now.getFullYear()}_${pad(now.getHours())}-${pad(now.getMinutes())}-${pad(now.getSeconds())}`;
+// }
 
-  const dbUser = process.env.DB_USER;
-  const dbPassword = process.env.DB_PASS;
-  const dbName = process.env.DB_NAME;
-  const dbHost = process.env.DB_HOST;
+// function backupDatabase() {
+//   const timestamp = getTimestamp();
+//   const backupDir = path.join(process.cwd(), 'database_backups');
+//   const backupPath = path.join(backupDir, `mysql_backup_${timestamp}.sql`);
 
-  if (!dbUser || !dbPassword || !dbName || !dbHost) {
-    console.error('Missing database credentials in environment variables.');
-    return;
-  }
+//   if (!fs.existsSync(backupDir)) {
+//     fs.mkdirSync(backupDir, { recursive: true });
+//   }
 
-  const command = `mysqldump -h ${dbHost} -u ${dbUser} -p'${dbPassword}' ${dbName} > ${backupPath}`;
+//   const dbUser = process.env.DB_USER;
+//   const dbPassword = process.env.DB_PASS;
+//   const dbName = process.env.DB_NAME;
+//   const dbHost = process.env.DB_HOST;
 
-  exec(command, error => {
-    if (!error) {
-      console.log('MySQL database backup created:', backupPath);
-    }
-  });
-}
+//   if (!dbUser || !dbPassword || !dbName || !dbHost) {
+//     console.error('Missing database credentials in environment variables.');
+//     return;
+//   }
 
-backupDatabase();
+//   const command = `mysqldump -h ${dbHost} -u ${dbUser} -p'${dbPassword}' ${dbName} > ${backupPath}`;
 
-setInterval(backupDatabase, 60 * 60 * 1000);
+//   exec(command, error => {
+//     if (!error) {
+//       console.log('MySQL database backup created:', backupPath);
+//     }
+//   });
+// }
+
+// backupDatabase();
+
+// setInterval(backupDatabase, 60 * 60 * 1000);
